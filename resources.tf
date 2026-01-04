149bb53edae44991d9632231e8a258f52c8e7db7
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-04 20:16:11 +0200

    update

--- resources.tf
+++ resources.tf
@@ -1,12 +1,13 @@
 
+
 ###  ---  Default Modules  ---  ###
 module "minio" {
   source = "./modules/minio"
    depends_on = [kubernetes_namespace.langsmith]
 }
 
-module "langsmith" {
-  source = "./modules/langsmith"
+module "argo_events" {
+  source = "./modules/argo_events"
   depends_on = [module.minio]
 }
 
