65235c58a5b39e659e9c8e8f20a52caf1649119a
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-04 20:16:58 +0200

    update

--- variable.tf
+++ variable.tf
@@ -4,10 +4,5 @@ variable "kubeconfig_path" {
   description = "Path to kubeconfig for the target cluster"
   default     = "~/.kube/config"
 }
-variable "langsmith_api_key_salt" {
-  description = "Stable secret used to hash LangSmith API keys. DO NOT ROTATE."
-  type        = string
-  sensitive   = true
-}
 
 
